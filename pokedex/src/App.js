c9c6ac55e740c197feeef0eabd4cde7fb7ff752b
Raphael Hetherington <raph37heth@gmail.com>
2020-02-11 20:11:22 +0000

    added pokedex and pokecard component

---
+++ pokedex/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Pokecard from './components/Pokecard';
+
+function App() {
+  return (
+    <div className="App">
+      <Pokecard />
+    </div>
+  );
+}
+
+export default App;
---
+++ pokedex/src/components/Pokecard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Pokecard() {
+    return (
+        <div>
+            pokecard
+        </div>
+    )
+}
+
+export default Pokecard
