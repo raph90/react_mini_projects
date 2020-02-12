7da114576996960e0d87eee158fa88cfbffdf677
Raphael Hetherington <raph37heth@gmail.com>
2020-02-12 19:46:31 +0000

    added dice

--- project4_clicker/src/components/Clicker.js
+++ project4_clicker/src/components/Clicker.js
@@ -1,19 +1,25 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 function Clicker() {
-    const [num, setNum] = useState(1)
+  const [num, setNum] = useState(1);
 
-    function handleClick(){
-        let number = Math.floor(1+ Math.random() * 11)
-        setNum(number)
-    }
+  function handleClick() {
+    let number = Math.floor(1 + Math.random() * 10) + 1;
+    setNum(number);
+  }
 
-    return (
-        <div>
-            Number is: {num}
-            <button onClick={handleClick}>Generate Random Number</button>
-        </div>
-    )
+  let winner = num === 7;
+
+  return (
+    <div>
+      Number is: {num}
+      {winner ? (
+        <h2>YOU WIN</h2>
+      ) : (
+        <button onClick={handleClick}>Generate Random Number</button>
+      )}
+    </div>
+  );
 }
 
-export default Clicker
+export default Clicker;
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # React Mini Projects
 
-This is a small group of mini projects for honing my React skills. I use these projects simply to focus on fundamentals. 
+This is a small group of mini projects for honing my React skills. I use these projects simply to focus on fundamentals.  
+
+1) [Dice](https://raph90.github.io/react_dice/)
